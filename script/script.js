29be0fe7cad7a777a8ea44673af6ad006ecb400b
Ragimov09 <57561604+Ragimov09@users.noreply.github.com>
2020-03-01 02:30:34 +0400

    RaTech v1

---
+++ script/script.js
@@ -0,0 +1,34 @@
+/********UP BUTTON********/
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function () {
+        $('body,html').animate({ scrollTop: 0 }, 800);
+    });
+});
+
+/********SIDE NAV********/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+/********SWIPER********/
+let swiper = new Swiper('.swiper-container', {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+});
